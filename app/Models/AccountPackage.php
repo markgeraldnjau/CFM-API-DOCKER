25ffa7bd7379320357c07b4ce78f1f84e9a908e4
Gerald Mark Njau <markgerald262@gmail.com>
2024-08-23 16:42:18 +0300

    Update AccountPackage.php

--- app/Models/AccountPackage.php
+++ app/Models/AccountPackage.php
@@ -11,31 +11,4 @@ class AccountPackage extends Model
 
     protected $table = 'customer_account_package_types';
 
-
-    // public function trainLine()
-    // {
-    //     return $this->belongsTo(MainLine::class, 'line_id', 'id');
-    // }
-    // public function zone()
-    // {
-    //     return $this->belongsTo(Zone::class, 'zone_id', 'id');
-    // }
-
-    // public function specialGroup()
-    // {
-    //     return $this->belongsTo(SpecialGroup::class, 'category_id', 'id');
-    // }
-
-    // public function cfmClass()
-    // {
-    //     return $this->belongsTo(CfmClass::class, 'class_package', 'id');
-    // }
-
-
-    // public function mainLine()
-    // {
-    //     return $this->belongsTo(MainLine::class, 'line_id', 'id');
-    // }
-
-
 }
